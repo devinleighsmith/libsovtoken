2100e93da07f2a3db2595b22568220c0de6d1d99
Devin Smith <devinleighsmith@gmail.com>
2021-05-20 12:10:58 -0700

    openssl workaround

--- libsovtoken/build_scripts/ios/mac/mac.02.libindy.env.sh
+++ libsovtoken/build_scripts/ios/mac/mac.02.libindy.env.sh
@@ -9,5 +9,7 @@ export RUST_TEST_THREADS=1
 
 #4) Setup OPENSSL_DIR variable: path to installed openssl library
 
-for i in `ls -t /usr/local/Cellar/openssl/`; do export OPENSSL_DIR=/usr/local/Cellar/openssl/$i; break; done
-#export OPENSSL_DIR=/usr/local/Cellar/openssl/1.0.2n   # path changes with version number
+if [ -z "${OPENSSL_DIR}" ]; then
+    export OPENSSL_DIR=$(brew --prefix openssl)
+    echo "OPENSSL_DIR not specified, using $OPENSSL_DIR"
+fi
